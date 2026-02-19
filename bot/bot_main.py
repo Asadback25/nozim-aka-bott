5b279382ab65752c11344b87773941fa59b7438f
asadback25 <asadback25@gmail.com>
2026-02-19 22:54:49 +0500

    Update bot_main: add sys.path modification for module discovery

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -1,9 +1,10 @@
 import os
-
+import sys
 from django.core.files.base import ContentFile
 from dotenv import load_dotenv
 load_dotenv()
 import django
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from django.utils import timezone
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
